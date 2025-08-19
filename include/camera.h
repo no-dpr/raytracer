a988e3127af3be3b245bc9138defab67d3cb1f45
Dominic Perno <dperno@mit.edu>
2025-08-18 23:27:02 -0700

    Added lambertian diffuse and gamma correction

--- include/camera.h
+++ include/camera.h
@@ -68,7 +68,7 @@ class camera {
             }
             hit_record rec;
             if (world.hit(r, interval(0.001, INF), rec)) {
-                vec3 direction = random_vector_on_hemisphere(rec.normal);
+                vec3 direction = rec.normal + random_unit_vector();
                 return 0.5 * ray_color(ray(rec.p, direction), world, depth - 1);
             }
 
