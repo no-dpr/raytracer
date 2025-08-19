015efe6830364c191c462ad4d7e8f654de81e2f5
Dominic Perno <dperno@mit.edu>
2025-08-19 02:12:33 -0700

    Implemented movable camera

--- include/camera.h
+++ include/camera.h
@@ -12,6 +12,12 @@ class camera {
         int     samples_per_pixel   = 10;   // Count of random samples for each pixel
         int     max_depth           = 10;   // Maximum number of ray bounces into scene
 
+        double  vfov                = 90;               // Vertical view angle
+        point3  lookfrom            = point3(0,0,0);    // Point camera is looking from
+        point3  lookat              = point3(0,0,-1);   // Point camera is looking at
+        vec3    vup                 = vec3(0,1,0);      // Camera-relative up direction
+
+
         void render(const volume& world) {
             initialize();
             std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -39,6 +45,7 @@ class camera {
         point3  pixel00_loc;            // Location of pixel 0, 0
         vec3    pixel_delta_u;          // Offset of pixel to the right
         vec3    pixel_delta_v;          // Offset of pixel below
+        vec3    u, v, w;                // Camera basis vectors
         
         void initialize() {
             image_height = int(image_width / aspect_ratio);
@@ -46,19 +53,25 @@ class camera {
 
             pixel_samples_scale = 1.0 / samples_per_pixel;
 
-            center = point3(0, 0, 0);
+            center = lookfrom;
 
-            auto focal_length = 1.0;
-            auto viewport_height = 2.0;
+            auto focal_length = (lookfrom - lookat).length();
+            auto theta = degrees_to_radians((vfov));
+            auto h = std::tan(theta/2);
+            auto viewport_height = 2 * h * focal_length;
             auto viewport_width = viewport_height * (double(image_width)/image_height);
 
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+
+            auto viewport_u = viewport_width * u;
+            auto viewport_v = -viewport_height * v;
 
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
 
-            auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+            auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
 
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
         }
